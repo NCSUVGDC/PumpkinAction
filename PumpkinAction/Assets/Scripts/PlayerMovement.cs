d5f555432c92472647558067b4f22c3c46644271
Griffin Geiger <griffin2420@yahoo.com>
2020-10-24 12:49:51 -0400

    removed meta files

--- PumpkinAction/Assets/Scripts/PlayerMovement.cs
+++ PumpkinAction/Assets/Scripts/PlayerMovement.cs
@@ -7,7 +7,9 @@ public class PlayerMovement : MonoBehaviour
 
     public CharacterController controller;
     public float speed = 12f;
+    public float gravity = -9.81f;
 
+    Vector3 velocity;
 
     // Start is called before the first frame update
     void Start()
@@ -24,5 +26,9 @@ public class PlayerMovement : MonoBehaviour
         Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity);
     }
 }
